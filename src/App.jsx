575173219bb80bb9cb12bb13d9070931070855e6
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-08-11 04:08:28 +0530

    added the app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TextEditor from './components/TextEditor';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Text Editor</h1>
+      <TextEditor />
+    </div>
+  );
+};
+
+export default App;
